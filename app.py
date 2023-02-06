abd497b6e33397db0871cb660e27198b3d3cdb89
Dina Abd El-Halim <91435311+Dinahalem@users.noreply.github.com>
2023-02-06 09:16:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,5 @@ def other_page(page_name):
                              % page_name, 404)
     return response
 
-if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+    app.run()
